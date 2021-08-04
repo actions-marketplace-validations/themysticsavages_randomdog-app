9eefa7a4ac0976ac0e1573ac43f5d599c708e270
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-08-04 13:45:10 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,2 @@
+import randomdog
+randomdog.push()
---
+++ randomdog/randomdog.py
@@ -0,0 +1,23 @@
+import base64
+import requests
+import datetime
+import json
+
+base = json.loads(open('config.json', 'r').read())
+url = 'https://raw.github.com/{}/{}/{}'.format(base['slugpath'], base['branch'], base['file'])
+dog = json.loads(requests.get('https://dog.ceo/api/breeds/image/random').text)['message']
+
+def push():
+    with open(base['file'], 'w', encoding='utf-8') as fh:
+        fh.write(requests.get(url).text.replace('/dog/', '[🐕 Random dog!]({})'.format(dog)))
+    fh.close()
+    sha = json.loads(requests.get('https://api.github.com/repos/{}/contents/README.md'.format(base['slugpath'])).text)['sha']
+    r = requests.put('https://api.github.com/repos/{}/contents/{}'.format(base['slugpath'], base['file']),
+    headers = {'Authorization': 'Token {}'.format(base['token'])},
+    json = {
+        'message': 'push {}, {}'.format(base['file'], datetime.date.today().strftime("%d/%m/%Y")),
+        'content': base64.b64encode(open(base['file'], 'r', encoding='utf-8').read().encode()).decode(),
+        'branch': base['branch'],
+        'sha': sha
+    })
+    print('executed sucessfully :)\n{}'.format(r.text))
